364b333121d4839097973d478a5c21d34277b644
NSP Software <nspsoft.info@gmail.com>
2026-02-04 12:46:43 +0700

    Fix: Add missing Indonesian password translation strings

---
+++ lang/id/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Password Anda telah diriset!',
+    'sent' => 'Kami sudah mengirimkan email yang berisi tautan untuk meriset password Anda!',
+    'throttled' => 'Harap tunggu sebelum mencoba lagi.',
+    'token' => 'Token untuk meriset password ini tidak valid.',
+    'user' => "Kami tidak dapat menemukan pengguna dengan alamat email tersebut.",
+
+];
